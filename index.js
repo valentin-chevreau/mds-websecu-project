245f2ae417ad81b286a6a97bdc37e2b691aa124c
Valentin CHEVREAU <valentin.chevreau@hetic.net>
2018-10-09 10:23:10 +0200

    dependencies: http, fs, express | get:/ with form

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const { createServer } = require('https');
 const { readFileSync } = require('fs');
 const express = require('express');
 
+//const { form } = require('./form.js');
+
 const app = express();
+
+
+app.get('/', function(req, res) {
+    res.send(`
+        <form action="/upload" enctype="multipart/form-data" method="post" autocomplete="off">
+        <input type="text" name="title">
+        <input type="file" name="file">
+        <input type="submit" value="Upload">
+        </form>`)
+});
+
 
 createServer(
     {
